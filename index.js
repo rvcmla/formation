87f018cf5f29000d8e65ae12ac48f1f7557b4d0d
bob <bob@bob.com>
2019-11-25 16:00:42 +0100

    ajout d'utils.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let fs = require('fs')
+let utils = require("./utils.js")
+
 var readline = require('readline'); //je crée une variable avec l'import
 
 const filename = "products.json";
@@ -7,19 +9,20 @@ const contents = fs.readFileSync(filenameWithCurrent, 'utf8')
 //console.log(contents)
 
 var obj;
+init();
 
 var rl = readline.createInterface({input : process.stdin, output: process.stdout, terminal: false});
 rl.on('line', function(line) {
     if (line.startsWith("I want product")) {
         var array = line.split(' ')
-        orderProductById(array[array.length-1], () => {
+        utils.orderProductById(obj, array[array.length-1], filenameWithCurrent, () => {
             getAllProducts()
         }) //Le deuxième paramètre est la fonction callback
     }
     
 })
 
-init();
+
 getAllProducts()
 
 function init() {
@@ -37,27 +40,9 @@ function getAllProducts() {
     })
 }
 
-function orderProductById(id, callback) {
-    var found;
-    obj.forEach((product) => {
-        if (product.id == id) {
-            product.orders_counter += 1;
-            console.log('Commande terminée. Voici votre fichier : ' + product.file_link)
-            found = true
-        }
-    })
-    if (found)
-        saveToFile(obj, callback)
-    else
-        callback()
-}
 
-function saveToFile(obj, callback) {
-    fs.writeFile(filenameWithCurrent, JSON.stringify(obj, null, 2), (err) => {
-        console.log('Sauvegardée en fichier.')
-        callback()
-    })
-}
+
+
 
 /*const contents = fs.readFile(`${__dirname}/products.json`, 'utf8', (err, content) => {
 console.log(contents)
