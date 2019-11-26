a861beeae4cec5ca84ad8170b033b049df5f10d3
bob <bob@bob.com>
2019-11-26 10:03:31 +0100

    création de modules products

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-let fs = require('fs')
 let utils = require("./utils.js")
+let products = require("./products.js")
 
+var obj = utils.retrieveData();
 var readline = require('readline'); //je crée une variable avec l'import
 
-//const contents = fs.readFileSync(utils.repository, 'utf8')
-//console.log(contents)
-
-var obj = utils.connectToRepository();
-//init();
 
 var rl = readline.createInterface({input : process.stdin, output: process.stdout, terminal: false});
 rl.on('line', function(line) {
-    if (line.startsWith(utils.phrase)) {
+    if (line.startsWith(products.phrase)) {
         var array = line.split(' ')
-        utils.orderProductById(obj, array[array.length-1], () => {
-        utils.getAllProducts(obj)
+        products.orderProductById(obj, array[array.length-1], () => {
+            products.getAllProducts(obj)
         }) //Le deuxième paramètre est la fonction callback
     }
     
 })
 
-utils.getAllProducts(obj)
-
-/*function init() {
-    try {
-        obj = JSON.parse(contents)
-    } catch (e) {
-        console.log(e)
-    }
-}*/
-
-/*const contents = fs.readFile(`${__dirname}/products.json`, 'utf8', (err, content) => {
-console.log(contents)
-try {
-    var obj = JSON.parse(contents)
-    console.log(obj)
-} catch (e) {
-    console.log(contents)
-}
-})
-*/
+products.getAllProducts(obj)
 
 
 
